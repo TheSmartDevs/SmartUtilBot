84ff5bbf2cf6ddfe0c174e40e2f71b78580b7525
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-29 07:29:01 +0600

    Delete core directory

--- core/__init__.py
+++
@@ -1,6 +0,0 @@
-#Copyright @ISmartDevs
-#Channel t.me/TheSmartDev
-from .mongo import user_activity_collection
-from .database import group_settings, auth_admins, banned_users
-from .db import group_channel_bindings
-from .start import setup_start_handler
