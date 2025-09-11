94aaf7d276f45308393724eba97467fd3c97ebb5
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-11 10:50:45 +0600

    Update bin.py

--- bot/modules/bin.py
+++ bot/modules/bin.py
@@ -1,6 +1,6 @@
 # Copyright @ISmartCoder
-#  SmartUtilBot - Telegram Utility Bot for Smart Features Bot 
-#  Copyright (C) 2024-present Abir Arafat Chawdhury <https://github.com/abirxdhack> 
+# SmartUtilBot - Telegram Utility Bot for Smart Features Bot 
+# Copyright (C) 2024-present Abir Arafat Chawdhury <https://github.com/abirxdhack> 
 from aiogram import Bot
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -18,9 +18,10 @@ from config import UPDATE_CHANNEL_URL
 from smartbindb import SmartBinDB
 import pycountry
 
-async def get_bin_info(bin: str):
+smartdb = SmartBinDB()
+
+async def get_bin_info(bin: str, message: Message):
     try:
-        smartdb = SmartBinDB()
         result = await smartdb.get_bin_info(bin)
         if result.get("status") == "SUCCESS" and result.get("data") and isinstance(result["data"], list) and len(result["data"]) > 0:
             return result
@@ -35,13 +36,18 @@ async def get_bin_info(bin: str):
 
 def get_flag(country_code: str):
     try:
+        if not country_code or len(country_code) < 2:
+            return "Unknown", ""
+        country_code = country_code.upper()
+        if country_code in ['US1', 'US2']:
+            country_code = 'US'
         country = pycountry.countries.get(alpha_2=country_code)
         if not country:
-            raise ValueError("Invalid country code")
+            return "Unknown", ""
         country_name = country.name
         flag_emoji = chr(0x1F1E6 + ord(country_code[0]) - ord('A')) + chr(0x1F1E6 + ord(country_code[1]) - ord('A'))
         return country_name, flag_emoji
-    except:
+    except Exception:
         return "Unknown", ""
 
 @dp.message(Command(commands=["bin"], prefix=BotCommands))
@@ -66,7 +72,7 @@ async def bin_handler(message: Message, bot: Bot):
             parse_mode=ParseMode.HTML
         )
         bin = args[0][:6]
-        bin_info = await get_bin_info(bin)
+        bin_info = await get_bin_info(bin, message)
         await delete_messages(message.chat.id, progress_message.message_id)
         if not bin_info:
             await send_message(
@@ -80,7 +86,7 @@ async def bin_handler(message: Message, bot: Bot):
         card_type = bin_info["data"][0].get("type", "Unknown")
         card_scheme = bin_info["data"][0].get("brand", "Unknown")
         bank_text = bank.upper() if bank else "Unknown"
-        country_code = bin_info["data"][0].get("country_code", "Unknown")
+        country_code = bin_info["data"][0].get("country_code", "")
         country_name, flag_emoji = get_flag(country_code)
         bin_info_text = (
             f"<b>🔍 BIN Details From Smart Database 📋</b>\n"
