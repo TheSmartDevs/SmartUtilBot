def6126bf87be77a3d2a0594074c72ba98ecb928
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-12-23 18:56:29 +0600

    Refactor V75.0(Beta)

--- bot/helpers/genbtn.py
+++ bot/helpers/genbtn.py
@@ -678,7 +678,7 @@ responses = {
     ),
     "about_me": (
         "<b>Name:</b> Smart Util\n"
-        "<b>Version:</b> v60.0 (Beta) 🛠\n\n"
+        "<b>Version:</b> v75.0 (Beta) 🛠\n\n"
         "<b>Development Team:</b>\n"
         "- <b>Creator:</b> <a href='https://t.me/ISmartCoder'>Abir Arafat Chawdhury 👨‍💻</a>\n"
         "- <b>Contributor:</b> <a href='https://t.me/nkka404'>Nyein Ko Ko Aung 🤝</a>\n"
