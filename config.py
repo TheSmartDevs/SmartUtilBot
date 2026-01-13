1b37f1d300b4d05f35027d4bbe37d75def17b9a4
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-01-14 00:55:24 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Copyright @ISmartCoder
-#  SmartUtilBot - Telegram Utility Bot for Smart Features Bot 
-#  Copyright (C) 2024-present Abir Arafat Chawdhury <https://github.com/abirxdhack> 
 import os
 from dotenv import load_dotenv
 
@@ -16,7 +13,7 @@ def get_env_or_default(key, default=None, cast_func=str):
             return default
     return default
 
-API_ID = get_env_or_default("API_ID", "Your_API_ID_Here")
+API_ID = get_env_or_default("API_ID", "Your_API_ID_Here", int)
 API_HASH = get_env_or_default("API_HASH", "Your_API_HASH_Here")
 BOT_TOKEN = get_env_or_default("BOT_TOKEN", "Your_BOT_TOKEN_Here")
 SESSION_STRING = get_env_or_default("SESSION_STRING", "Your_SESSION_STRING_Here")
@@ -27,8 +24,8 @@ DATABASE_URL = get_env_or_default("DATABASE_URL", "Your_DATABASE_URL_Here")
 OPENAI_API_KEY = get_env_or_default("OPENAI_API_KEY", "Your_OPENAI_API_KEY_Here")
 REPLICATE_API_TOKEN = get_env_or_default("REPLICATE_API_TOKEN", "Your_REPLICATE_API_TOKEN_Here")
 GOOGLE_API_KEY = get_env_or_default("GOOGLE_API_KEY", "Your_GOOGLE_API_KEY_Here")
-OCR_API_KEY = get_env_or_default("OCR_API_KEY", "Your_OCR_API_KEY_Here")
 TRANS_API_KEY = get_env_or_default("TRANS_API_KEY", "Your_TRANS_API_KEY_Here")
+OCR_API_KEY = get_env_or_default("OCR_API_KEY", "Your_OCR_API_KEY_Here")
 MODEL_NAME = get_env_or_default("MODEL_NAME", "gemini-2.0-flash")
 CC_SCRAPPER_LIMIT = get_env_or_default("CC_SCRAPPER_LIMIT", 5000, int)
 SUDO_CCSCR_LIMIT = get_env_or_default("SUDO_CCSCR_LIMIT", 10000, int)
@@ -37,25 +34,26 @@ MAIL_SCR_LIMIT = get_env_or_default("MAIL_SCR_LIMIT", 10000, int)
 SUDO_MAILSCR_LIMIT = get_env_or_default("SUDO_MAILSCR_LIMIT", 15000, int)
 CC_GEN_LIMIT = get_env_or_default("CC_GEN_LIMIT", 2000, int)
 MULTI_CCGEN_LIMIT = get_env_or_default("MULTI_CCGEN_LIMIT", 5000, int)
-A360APIBASEURL = get_env_or_default("A360APIBASEURL", "https://a360api-c8fbf2fa3cda.herokuapp.com")
+TEXT_MODEL = get_env_or_default("TEXT_MODEL", "deepseek-r1-distill-llama-70b")
+GROQ_API_URL = get_env_or_default("GROQ_API_URL", "https://api.groq.com/openai/v1/chat/completions")
+FILE_WORKER_URL = get_env_or_default("FILE_WORKER_URL", "Your_FILE_WORKER_URL_Here")
+FILE_API_URL = get_env_or_default("FILE_API_URL", "Your_FILE_API_URL_Here")
 GROQ_API_KEY = get_env_or_default("GROQ_API_KEY", "Your_GROQ_API_KEY_Here")
-WEB_SS_KEY = get_env_or_default("WEB_SS_KEY", "Your_WEB_SS_KEY_Here")
-IMAGE_UPLOAD_KEY = get_env_or_default("IMAGE_UPLOAD_KEY", "Your_IMAGE_UPLOAD_KEY_Here")
-GROQ_API_URL = get_env_or_default("GROQ_API_URL", "https://api.groq.com/openai/v1/chat/completions")
-TEXT_MODEL = get_env_or_default("TEXT_MODEL", "qwen/qwen3-32b)
-IPINFO_API_TOKEN = get_env_or_default("IPINFO_API_TOKEN", "Your_IPINFO_API_TOKEN_Here")
+A360APIBASEURL = get_env_or_default("A360APIBASEURL", "https://a360api.vercel.app")
+UPDATE_CHANNEL_URL = get_env_or_default("UPDATE_CHANNEL_URL", "https://t.me/TheSmartDev")
+LOG_CHANNEL_ID = get_env_or_default("LOG_CHANNEL_ID", "Your_LOG_CHANNEL_ID_Here", int)
 raw_prefixes = get_env_or_default("COMMAND_PREFIX", "!|.|#|,|/")
 COMMAND_PREFIX = [prefix.strip() for prefix in raw_prefixes.split("|") if prefix.strip()]
-UPDATE_CHANNEL_URL = get_env_or_default("UPDATE_CHANNEL_URL", "https://t.me/TheSmartDev")
-FILE_API_URL = get_env_or_default("FILE_API_URL", "https://0a740c0f-d312-4.com")
-LOG_CHANNEL_ID = get_env_or_default("LOG_CHANNEL_ID", "-1002735511721")
+DOMAIN_CHK_LIMIT = get_env_or_default("DOMAIN_CHK_LIMIT", 20, int)
+PROXY_CHECK_LIMIT = get_env_or_default("PROXY_CHECK_LIMIT", 20, int)
 IMGAI_SIZE_LIMIT = get_env_or_default("IMGAI_SIZE_LIMIT", 5242880, int)
 MAX_TXT_SIZE = get_env_or_default("MAX_TXT_SIZE", 15728640, int)
 MAX_VIDEO_SIZE = get_env_or_default("MAX_VIDEO_SIZE", 2147483648, int)
 YT_COOKIES_PATH = get_env_or_default("YT_COOKIES_PATH", "bot/SmartCookies/SmartUtilBot.txt")
 VIDEO_RESOLUTION = get_env_or_default("VIDEO_RESOLUTION", "1280x720", lambda x: tuple(map(int, x.split('x'))))
-DOMAIN_CHK_LIMIT = get_env_or_default("DOMAIN_CHK_LIMIT", 20, int)
-PROXY_CHECK_LIMIT = get_env_or_default("PROXY_CHECK_LIMIT", 20, int)
+IMAGE_UPLOAD_KEY = get_env_or_default("IMAGE_UPLOAD_KEY", "Your_IMAGE_UPLOAD_KEY_Here")
+IPINFO_API_TOKEN = get_env_or_default("IPINFO_API_TOKEN", "Your_IPINFO_API_TOKEN_Here")
+WEB_SS_KEY = get_env_or_default("WEB_SS_KEY", "Your_WEB_SS_KEY_Here")
 
 required_vars = {
     "API_ID": API_ID,
